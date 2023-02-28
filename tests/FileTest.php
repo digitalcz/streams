abd4387b1a3322579a2ef71fb15d378946392b6f
Pavel Stejskal <spajxo@gmail.com>
2023-02-28 07:51:55 +0100

    Improve test coverage

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -27,4 +27,13 @@ class FileTest extends TestCase
         $file->delete();
         self::assertFileDoesNotExist($file->getPath());
     }
+
+    public function testTemp(): void
+    {
+        $file = File::temp();
+
+        self::assertStringStartsWith(sys_get_temp_dir(), $file->getPath());
+        self::assertSame('plainfile', $file->getMetadata('wrapper_type'));
+        self::assertSame('STDIO', $file->getMetadata('stream_type'));
+    }
 }
